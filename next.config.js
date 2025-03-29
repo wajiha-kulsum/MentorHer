400d45d1ed465f21c1416f25a26898e06289070c
KUMUDZIMAL <kumud.223133206@vcet.edu.in>
2025-03-29 12:57:20 -0700

    done

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.externals.push({
-        'bufferutil': 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      });
-      return config;
-    },
-    async headers() {
-      return [
-        {
-          source: "/api/socket",
-          headers: [
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          ],
-        },
-      ];
-    },
-    eslint: {
-      // This allows production builds to successfully complete even if there are ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // !! WARNING !!
-      // This allows production builds to successfully complete even if
-      // your project has TypeScript errors.
-      // !! WARNING !!
-      ignoreBuildErrors: true,
-    },
-    experimental: {
-      optimizeFonts: false,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  webpack: (config) => {
+    config.externals.push({
+      bufferutil: "bufferutil",
+      "utf-8-validate": "utf-8-validate",
+    });
+    config.resolve.alias["@tailwindcss/oxide-linux-x64-gnu"] = false;
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/socket",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+        ],
+      },
+    ];
+  },
+  eslint: {
+    // This allows production builds to successfully complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARNING !!
+    // This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    // !! WARNING !!
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    // @ts-ignore: nextConfig.experimental.optimizeFonts is not officially typed but works at runtime.
+    optimizeFonts: false,
+  },
+};
+
+module.exports = nextConfig;
